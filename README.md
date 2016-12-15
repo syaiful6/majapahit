d59a1e322fe3aa5ccabd131da3a579fcfeb3be07
Syaiful Bahri <syaiful@pengunjungblog.com>
2016-12-16 00:47:19 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+### Majapahit
+
+Majapahit is a simple web accounting that intended to track my financial.
+
+Majapahit currently in dev.
---
+++ app/devel.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE PackageImports #-}
+import "majapahit" Application (develMain)
+import Prelude (IO)
+
+main :: IO ()
+main = develMain
